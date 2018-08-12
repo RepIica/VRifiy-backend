15fd82c8ac6d17437d0b1f1267fec78d8e3f22ed
Sammy Akharaz <ketrixz@gmail.com>
2018-08-11 21:28:58 -0400

    updated to use email for login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AuthController < ApplicationController
 
   def login
-    user = User.find_by(name: params[:name])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       render json: { token: issue_token({ id: user.id }) }
     else
