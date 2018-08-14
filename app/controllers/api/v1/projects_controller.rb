2862cfff9e2815c9f7d62ac2bae43f5adafabc00
Sammy Akharaz <ketrixz@gmail.com>
2018-08-14 10:10:40 -0400

    project controller updates, persists project and project path

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    # @projects = Project.all
+    # render json: @projects
+    @projects = Project.all.where(user_id: params[:userId]).map { |e| e.name  }
     render json: @projects
   end
 
   def show
-    @project = Project.find(params[:id])
-    render json: @project
+    user = User.all.find(params[:userId])
+    @projects = user.projects.map { |e| e.name  }
+    render json: @projects
   end
 
   def create
+    user = User.all.find(params[:userId])
+    dir = "../VRify-frontend/public/#{user.name}"
+    # dir = "../../../../../VRify-frontend/public/#{user.name}"
+    Dir.mkdir dir unless File.exist? dir
+
     file = params[:fileContent]
-    newFile = File.open("#{params[:name]}.txt","w")
+    newFile = File.open(File.expand_path("#{dir}/#{params[:name]}.html"),"w")
     newFile.write(file)
-    project = Project.create(name: params[:name], filepath: params[:filepath], user_id: params[:userId])
+    project = Project.create(name: params[:name], filepath: "./#{user.name}/#{params[:name]}.html", user_id: params[:userId])
     if project.valid?
       render json: project
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :projects
+      resources :projects, only: [:create,:update,:show]
       resources :users, only: [:index, :create, :show]
 
-      post "/auth", to: "auth#login" # login
-      get "/current_user", to: "auth#get_current_user" # ???
+      post "/auth", to: "auth#login"
+      get "/current_user", to: "auth#get_current_user"
+
+      post "/user/projects", to: "projects#index"
 
     end
   end
